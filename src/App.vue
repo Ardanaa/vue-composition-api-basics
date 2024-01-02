cf42559fc0df26014b0344de287f73b31f9ff0cc
Ardana <ardana733@gmail.com>
2024-01-02 19:27:30 +0700

    first cimmit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full h-full ">
+    <div class="flex justify-center">
+      <nav class="flex gap-2">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+
+    <div class="mx-auto mt-10">
+      <RouterView />
+    </div>
+  </div>
+</template>
